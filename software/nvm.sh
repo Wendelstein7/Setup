bde71365984187f7f02067d93aa9af6c7cc3b63d
Wendelstein7 <23213464+Wendelstein7@users.noreply.github.com>
2022-05-30 13:24:58 +0200

    nvm

--- software/nvm.sh
+++ software/nvm.sh
@@ -2,8 +2,13 @@
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 
-source ~/.bashrc
+# Fix to directly use nvm in this script:
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm install node
-
 nvm use node
+
+echo
+echo "Installation complete, npm and node should be available in new terminal sessions."
